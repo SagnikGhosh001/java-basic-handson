11bcadb3402a86f9d6a4775fbcd72ec46b0f413d
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-04-27 15:17:02 +0530

    solved

--- Money.java
+++ Money.java
@@ -1,1 +1,17 @@
 // Money.java
+
+class Money {
+  final int value;
+
+  Money(int value) {
+    this.value = value;
+  }
+
+  Money add(Money adderMoney) {
+    return new Money(this.value + adderMoney.value);
+  }
+
+  Money minus(Money adderMoney) {
+    return new Money(this.value - adderMoney.value);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -8,4 +8,29 @@ class StringUtils {
 
     return false;
   }
+
+  static String reverse(String str) {
+    String reversed = "";
+
+    for(int i = str.length() - 1; i >= 0; i--) {
+      reversed += str.charAt(i);
+    }
+
+    return reversed;
+  }
+
+  static String replaceAll(String str, char character,char replacer) {
+    char[] reversed = new char[str.length()];
+
+    for(int i = 0;  i < str.length(); i++) {
+
+      if(str.charAt(i) == character) {
+        reversed[i] = replacer;
+        continue;
+      }
+      reversed[i] = str.charAt(i);
+    }
+
+    return new String(reversed);
+  }
 }
