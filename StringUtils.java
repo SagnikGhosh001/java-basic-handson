f8b97301b14a6a0fffcf37564178202833fd0de8
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-04-27 14:53:42 +0530

    solved first one

--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,11 @@
 // StringUtils
+
+class StringUtils {
+  static boolean containsChar(String source, char character) {
+    for(int i = 0; i < source.length(); i++) {
+      if(source.charAt(i) == character) return true;
+    }
+
+    return false;
+  }
+}
